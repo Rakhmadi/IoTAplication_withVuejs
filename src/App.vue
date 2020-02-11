d11de0eafacbaf58240fc7b9419c435f885bccfe
rakhmadi D <rakhmadiwalker@gmail.com>
2020-02-11 17:16:17 +0700

    bug fixed and more bug

--- src/App.vue
+++ src/App.vue
@@ -15,10 +15,10 @@
         </v-list-item>
         <v-list-item link>
           <v-list-item-action>
-            <v-icon>mdi-contact-mail</v-icon>
+           
           </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Contact</v-list-item-title>
+          <v-list-item-content  v-on:click="dialog = !dialog">
+            <v-list-item-title> Ip Setting</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -37,6 +37,8 @@
         
         fluid
       >
+      
+
        <v-card
     class="mx-auto"
     style="
@@ -51,7 +53,7 @@
     <v-list-item three-line>
       <v-list-item-content>
 
-        <v-list-item-title class="headline mb-1"></v-list-item-title>
+      
         <v-list-item>
         <v-list-item-icon>
         <v-icon>mdi-water</v-icon>
@@ -92,6 +94,7 @@ background: rgba(58,28,113,1) ;
   >
     <v-list-item three-line>
       <v-list-item-content>
+         
         <v-list-item-title class="headline mb-1">Relay</v-list-item-title>
         <v-list-item-subtitle>Status Relay Condition <h2 v-if="rely === 1">OFF</h2><h2 v-if="rely === 0">ON</h2> </v-list-item-subtitle>
       </v-list-item-content>
@@ -113,26 +116,46 @@ background: rgba(58,28,113,1) ;
           margin-right:1px;
           "
         >
-       
+     
   
         </v-row>
         
       </v-container>
     </v-content>
+     <v-dialog v-model="dialog" persistent max-width="360">
+     
+      <v-card dark
+       style="
+     margin-bottom:10px;
+        background: rgb(58,28,113);
+          background: linear-gradient(137deg, rgba(58,28,113,1) 0%, rgba(215,109,119,1) 100%);
+     "
+      >
+        <v-card-title class="headline">Setting Ip</v-card-title>
+        <v-card-text>  <v-text-field label="IP" :placeholder="ips" v-model="ip" hide-details="auto"></v-text-field></v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn  text @click="dialog = false">close</v-btn>
+          <v-btn text @click="save()">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
   </v-app>
+  
 </template>
 
 <script>
+ 
 import axios from 'axios';
   export default {
     props: {
       source: String,
     },
-  
+    
     data(){
       return{
         dats:{
-         
         },
          drawer: null,
          g:'off',
@@ -141,40 +164,51 @@ import axios from 'axios';
           signalstr:null,
           condition:'',
           noe:'',
-          rely:''
+          rely:'',
+          pending:false,
+             ip: '', // or null
+            port: '8888', // or null
+            ips:null,
+            dialog: false
 
       }
     },
     mounted() {
+      
       setInterval(() => {
+        var url =localStorage.getItem('ip_device');
       const yu =this;
-    axios.get("http://192.168.43.100/dht").then(function(resp){
+      yu.ips=localStorage.getItem('ip_device');
+     
+    axios.get(`http://${url}/dht`).then(function(resp){
       yu.dats=resp.data;
       yu.kelembapan=resp.data.kelembapan
       yu.temperatur=resp.data.temperatur
       yu.signalstr=resp.data.signalstr
-
-    }).catch(function(err){
-      yu.dats=err.response;
-    })
-   }, 500);
+    }).catch(error => console.log(error)).finally(function () { yu.pending = true })
+   }, 1000);
       setInterval(() => {
       const yu =this;
-    axios.get("http://192.168.43.100/").then(function(resp){
+      var url =localStorage.getItem('ip_device');
+    axios.get(`http://${url}/`).then(function(resp){
      yu.noe=resp.data
      yu.rely=resp.data.rely
-
-
     }).catch(function(err){
       yu.noe=err.response;
       console.log(err.response);
     })
-   }, 500);
+   }, 1000);
     },
     methods: {
+        save(){
+          var t = this;
+           localStorage.setItem('ip_device',t.ip );
+           console.log(t.ip)
+        },
        on(){
          const yu =this;
-      axios.get("http://192.168.43.100/relay",{ params: { condition:"on" }}).then(function(resp){
+         var url =localStorage.getItem('ip_device');
+      axios.get(`http://${url}/relay`,{ params: { condition:"on" }}).then(function(resp){
       yu.g=resp.data;
       yu.condition=resp.data.condition
 
@@ -185,14 +219,16 @@ import axios from 'axios';
     },
     off(){
          const yu =this;
-      axios.get("http://192.168.43.100/relay",{ params: { condition:"off" }}).then(function(resp){
+         var url =localStorage.getItem('ip_device');
+      axios.get(`http://${url}/relay`,{ params: { condition:"off" }}).then(function(resp){
       yu.g=resp.data;
        yu.condition=resp.data.condition
 
     }).catch(function(err){
       yu.g=err.response;
     })
-    }
+    },
+   
     },
   }
 
